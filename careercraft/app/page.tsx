5db269baa4c9b767376e38caf9cba04d3a214b8a
Mahesh <maheshmallapu2309@gmail.com>
2025-03-19 15:45:29 +0530

    Changes Do e

--- careercraft/app/page.tsx
+++ careercraft/app/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Hero from '@/components/Hero'
-const page = () => {
+
+const Page = () => {
   return (
-    <div>
-      <Hero/>
-    </div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
+    </main>
   )
 }
 
-export default page
+export default Page;
